9ef9ab025a02c9bda6afc1ab09797301a4c402e8
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-17 03:27:43 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,8 +12,10 @@ class Triangle
     # sum of lengths of 2 sides always exceed length of third side
     # each side must be larger than 0
     
+    positive_length = (@side_1 > 0) && (@side_2 > 0) && (@side_3)
     triangle_inequality = (@side_1+@side_2 >= @side_3) || (@side_1+@side_3 >= @side_2) || (@side_2+@side_3 >= @side_1) 
     
+    raise TriangleError unless positive_length && triangle_inequality
     
     if side_1 == side_2 == side_3 
       :equilateral #equal sides
@@ -23,7 +25,7 @@ class Triangle
     # scalene triangles have no equal sides
     else if side_1 != side_2 != side_3
       :scalene
-    
+    end
     
     # if ...
     #   begin
@@ -38,7 +40,9 @@ class Triangle
     
   # Custom error class, `TriangleError`  inherits from `StandardError`:
   class TriangleError < StandardError 
-    
+    def message
+    "This is an illegal triangle"
+    end
   end
 end
 

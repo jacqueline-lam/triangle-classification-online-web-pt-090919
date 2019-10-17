ed18f38b2dcb10d17d1352f7016b41ef711fb025
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-17 03:46:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -42,44 +42,8 @@ class Triangle
   # Custom error class, `TriangleError`  inherits from `StandardError`:
   class TriangleError < StandardError 
     def message
-      "This is an invalid triangle"
+      p "This is an invalid triangle"
     end
   end
   
 end
-
-
-# class Triangle
-  
-#   def initialize(side_1,side_2,side_3)
-#     @side_1 = side_1
-#     @side_2 = side_2
-#     @side_3 = side_3
-#   end
-  
-#   def kind()
-#     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-#       raise TriangleError
-#     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
-#       raise TriangleError
-#     else
-#       if (@side_1 == @side_2) && (@side_2 == @side_3)
-#         :equilateral
-#       elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
-#         :isosceles
-#       elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
-#         :scalene
-#       elsif (@side_1 == false) && (@side_2 == false) && (@side_3 == false)
-#       raise TriangleError
-#       end
-#     end
-
-#   end
-  
-# end
-
-# class TriangleError < StandardError
-#   def message
-#     "This is an illegal triangle"
-#   end
-# end

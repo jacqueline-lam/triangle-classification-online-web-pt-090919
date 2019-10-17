8138e106244e6c56a62e05f0b6eb2b46989d5cd9
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-17 03:34:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
     elsif @side_1 == @side_2 || @side_2 = @side_3 || @side_1 == side_3
       :isosceles
     # scalene triangles have no equal sides
-    else if @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
       :scalene
     end
     
